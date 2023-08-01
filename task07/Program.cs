d3681da4b6802813fc3f7bf9079daec49d94e484
Oleg Petrukhin <petrukhinoleg03@gmail.com>
2023-08-01 12:26:49 +0300

    task07 трехзначное число

---
+++ task07/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number>99 && number<1000)
+{
+    Console.WriteLine(number%10);
+}
+else
+{
+    Console.Write("Число не трехзначное");
+}
