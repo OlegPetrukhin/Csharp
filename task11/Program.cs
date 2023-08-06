7bd4d4eddec140934f2a8c0d6e46081d296ea128
Oleg Petrukhin <petrukhinoleg03@gmail.com>
2023-08-06 16:33:58 +0300

    task11 удалениевторого числа в случайном трехзначном числе

---
+++ task11/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+Напишите программу которая вводит случайное 
+трехзначное число и удаляет вторую цифру этого числа
+*/
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+int number1 = number / 100;
+Console.WriteLine(number1);
+int number3 = number % 10;
+Console.WriteLine(number3);
+int result = number1 * 10 + number3;
+Console.Write(result);
