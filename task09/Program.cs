4592646a12079c225e1527100f4426fe3e27c9ed
Oleg Petrukhin <petrukhinoleg03@gmail.com>
2023-08-06 16:12:44 +0300

    task09 наибольшая цифра числа

---
+++ task09/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Напишите программу которая выводит случайное число из отрезка [10,99]
+и показывает наибольшую цифру числа.
+*/
+int number = new Random().Next(10, 100);
+Console.WriteLine(number);
+int number1 = number / 10;
+int number2 = number % 10;
+if (number1 > number2)
+{
+    Console.Write($"наибольшая цифра числа {number} - {number1}");
+}
+else
+{
+    Console.Write($"наибольшая цифра числа {number} - {number2}");
+}
