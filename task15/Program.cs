2a9e122ce0b2b8e961aedf149373b3234db72832
Oleg Petrukhin <petrukhinoleg03@gmail.com>
2023-08-07 17:42:34 +0300

    ТАСК 15 Проверка на выходной день

---
+++ task15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите цифру обозначающую день недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number==6 | number==7)
+{
+    Console.Write($"{number}-Выходной день!");
+}
+else if (number>=1 && number<6)
+{
+   Console.Write($"{number}-Рабочий день:("); 
+}
+else
+{
+   Console.Write($"{number}-Нет такого дня недели."); 
+}
