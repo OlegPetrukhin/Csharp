1ba26380afb9f841bb97ba715f320ccddda171dd
Oleg Petrukhin <petrukhinoleg03@gmail.com>
2023-08-07 17:27:56 +0300

    task13 Найти третью цифру в числе

---
+++ task13/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int FinNumber = number;
+if (FinNumber / 100 == 0)
+{
+    Console.Write("Третьей цифры нет");
+}
+else
+{
+    while (FinNumber > 999)
+    {
+        FinNumber = FinNumber / 10;
+    }
+    Console.Write(FinNumber % 10);
+}
