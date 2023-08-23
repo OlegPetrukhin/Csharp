3b1009a898d3a3aad049d286327b4867ea04603e
Oleg Petrukhin <petrukhinoleg03@gmail.com>
2023-08-23 12:24:23 +0300

    task19 проверка на палиндром

---
+++ task19/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+System.Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int number0 = number;
+int mirrow = 0;
+int number1 = 10000;
+if (number > 9999 && number < 100000)
+{
+    for (int i=0;i<=5;i++)
+    {
+        mirrow += (number%10)*number1;
+        number = number/10;
+        number1 = number1/10;
+    }
+    if (number0 == mirrow)
+    {
+        System.Console.WriteLine("ДА");
+    }
+    else System.Console.WriteLine("НЕТ");
+}
+else System.Console.WriteLine("Число не пятизначное");
