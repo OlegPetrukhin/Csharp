64658bfecba315191cfb431efa87f83a6b7d9d7a
Oleg Petrukhin <petrukhinoleg03@gmail.com>
2023-08-23 12:45:36 +0300

    task23 таблица кубов

---
+++ task23/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+double result = 0;
+for (int i = 1;i<=numberN; i++ )
+{
+    result = Math.Pow(i,3);
+    System.Console.Write(result + " ");
+}
